9c0d88107de15ad6d4dbcad9cbe9cefa1304aa2e
AlexandreSout0 <soutoinf@gmail.com>
2023-09-22 13:32:23 -0300

    TopAppBar

--- app/src/main/java/com/alx/learncompose/Scaffold.kt
+++ app/src/main/java/com/alx/learncompose/Scaffold.kt
@@ -1,23 +1,36 @@
 package com.alx.learncompose
 
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,17 +40,38 @@ fun Scaffold() {
     
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar() {
-    appBar = {
+fun MyTopAppBar(modifier: Modifier = Modifier) {
         TopAppBar(
-            title = { Text("Backdrop") },
-            navigationIcon = {
-                IconButton( onClick = { } ) {
+            title = {
+                Box(modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center) {
+                    Text(text = "Top App Bar", fontWeight = FontWeight.Bold)
                 }
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = Color(0xFF9F271C)
+            ),
+            navigationIcon = {
+                IconButton(onClick = {  }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back" )
+                }
+            },
+            actions = {
+                IconButton(onClick = {  }) {
+                    Icon(imageVector = Icons.Filled.Search , contentDescription = "Search")
+                }
+                IconButton(onClick = { }) {
+                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = "Date")
+                }
+                
             }
+
         )
     }
-}
+
+
+
+
+
