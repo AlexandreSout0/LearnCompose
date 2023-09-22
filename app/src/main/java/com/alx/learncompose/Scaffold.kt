726435ab11064e99f530b6db8c76f69e1a5dac11
AlexandreSout0 <soutoinf@gmail.com>
2023-09-22 14:31:41 -0300

    MyBottomNavigationBar

--- app/src/main/java/com/alx/learncompose/Scaffold.kt
+++ app/src/main/java/com/alx/learncompose/Scaffold.kt
@@ -11,7 +11,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.ShoppingCart
@@ -20,12 +23,19 @@ import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,41 +46,97 @@ import androidx.compose.ui.unit.dp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Scaffold() {
-    Scaffold( topBar = { MyTopAppBar() }) {
+    Scaffold(topBar = { MyTopAppBar() }, bottomBar = { MyBottomNavigationBar()}) {
     
+        
     }
+    
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopAppBar(modifier: Modifier = Modifier) {
-        TopAppBar(
-            title = {
-                Box(modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center) {
-                    Text(text = "Top App Bar", fontWeight = FontWeight.Bold)
-                }
-            },
-            colors = TopAppBarDefaults.smallTopAppBarColors(
-                containerColor = Color(0xFF9F271C)
-            ),
-            navigationIcon = {
-                IconButton(onClick = {  }) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back" )
-                }
-            },
-            actions = {
-                IconButton(onClick = {  }) {
-                    Icon(imageVector = Icons.Filled.Search , contentDescription = "Search")
-                }
-                IconButton(onClick = { }) {
-                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = "Date")
-                }
-                
+    TopAppBar(
+        title = {
+            Box(
+                modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Top App Bar", fontWeight = FontWeight.Bold)
             }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Color.Red,
+            scrolledContainerColor = Color.White,
+            navigationIconContentColor = Color.White,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White,
+        ),
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+            }
+        },
+        actions = {
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+            }
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "Date")
+            }
+            
+        }
+    
+    )
+}
 
+@Composable
+fun MyBottomNavigationBar(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier
+            .padding(10.dp)
+            .clip(RoundedCornerShape(20.dp)),
+            containerColor = Color.Red,
+    ) {
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home") },
+            label = { Text(text = "Home") },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = Color.Gray,
+                unselectedIconColor = Color.White,
+                selectedTextColor = Color.White,
+                unselectedTextColor = Color.White,
+                indicatorColor = Color.White)
+            )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = "Fav") },
+            label = { Text(text = "Fav") },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = Color.Gray,
+                unselectedIconColor = Color.White,
+                selectedTextColor = Color.White,
+                unselectedTextColor = Color.White,
+                indicatorColor = Color.White)
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(imageVector = Icons.Default.Person, contentDescription = "Person") },
+            label = { Text(text = "Person") },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = Color.Gray,
+                unselectedIconColor = Color.White,
+                selectedTextColor = Color.White,
+                unselectedTextColor = Color.White,
+                indicatorColor = Color.White)
         )
     }
-
+    
+}
 
 
 
